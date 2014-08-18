660f35724714d6d9ba07045110eade59abb9d245
ReyesMagosSoft.co <reyesmagossoft.co@gmail.com>
2014-08-17 22:46:25 -0500

    session authorized

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -50,6 +50,8 @@ module.exports = {
 	index: function (req, res, next) {
 		// body...
 		//get an array of all users
+		console.log(new Date());
+		console.log(req.session.authenticated);
 		User.find(function foundUsers (err, users) {
 			if(err) return next(err);
 
